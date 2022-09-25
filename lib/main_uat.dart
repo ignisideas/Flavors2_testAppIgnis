881583f7c39199f1c30d1ac25dd6fe711c9a30f7
AndresTapieroVanegas <andrestapierovanegas@gmail.com>
2022-09-25 10:13:46 -0500

    UAT setup

--- lib/main_uat.dart
+++ lib/main_uat.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'firebase/uat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app_config.dart';
 import 'home.dart';
 
-void main() {
+void main() async {
   // 1
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   const configuredApp = AppConfig(
     child: MyApp(),
     // 2
