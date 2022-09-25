61637b8d738351ede63556025361bcf345798761
AndresTapieroVanegas <andrestapierovanegas@gmail.com>
2022-09-25 10:28:03 -0500

    google-services fix for DEV and UAT + MainActivity.KT

--- android/app/src/main/kotlin/com/ignisideas/flavors2_testappignis/MainActivity.kt
+++ android/app/src/main/kotlin/com/ignisideas/flavors2_testappignis/MainActivity.kt
@@ -1,6 +1,10 @@
-package com.ignisideas.flavors2_testappignis
-
+package com.ashok.flutterEX
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
